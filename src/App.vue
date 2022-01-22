b02c209f03d5324674863ce67f00df462cf6c3f4
Paul den Boer <paul@levarne.nl>
2022-01-22 15:54:38 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AboutPaul from './components/AboutPaul.vue'
+
+</script>
+
+<template lang="pug">
+.cv-project
+  AboutPaul
+
+
+</template>
+
+<style lang="scss" scoped>
+.cv-project{
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/AboutPaul.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed, reactive, ref, watch } from "vue";
+
+interface personObject {
+	name?: string;
+	github?: string;
+	linkedIn?: string;
+}
+
+const paulsInfo: personObject = reactive({
+	name: "Paul den Boer",
+	github: "https://www.github.com/Pauldb-Levarne",
+	linkedIn: "https://www.linkedin.com/in/pauwlusdenboer/",
+});
+
+let newName = ref(undefined);
+let nameLength = computed(() => paulsInfo.name?.length  );
+watch(
+	() => newName.value,
+	() => {
+		console.log(newName.value);
+	}
+);
+</script>
+<template lang="pug">
+.about-paul
+  h1 {{ paulsInfo.name }}
+  a(:href='paulsInfo.github') Click here for my Github
+  br
+  a(:href='paulsInfo.linkedIn') Click here for my LinkedIn
+  br
+  .enter-values
+    input(type="text" placeholder="Enter a new name" v-model='newName')
+    button(@click="paulsInfo.name = newName " :disabled='newName === undefined') Change the name!
+
+</template>
+<style lang="scss" scoped>
+.about-paul {
+	font-family: "Open Sans";
+	h1 {
+		font-size: 4rem;
+	}
+}
+</style>
